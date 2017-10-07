373a5fe261d9d9dcaa7f677f1f9429db29c812b1
Unknown <alessavolpe@gmail.com>
2017-10-07 13:42:19 -0400

    Add MHI code and MHI output image

--- ps7_matlab/ps7.m
+++ ps7_matlab/ps7.m
@@ -1,4 +1,4 @@
-%Motion history
+%Binary sequence
 clear all; close all; clc;
 readerobj = VideoReader('input/PS7A1P1T1.avi');
 vidFrames = read(readerobj);
@@ -19,7 +19,7 @@ BW = B > 15;
      %imshow(BW(:,:,j));
      M(j) = getframe;
  end
- 
+ close all;
   % Create a figure
        %hf = figure; 
        
@@ -29,7 +29,23 @@ BW = B > 15;
        % Playback movie once at the video's frame rate
        %movie(hf, M, 1, readerobj.FrameRate);
        
-       v = VideoWriter('binary.avi');
-       open(v)
-       writeVideo(v,M);
-       close(v)
+       %v = VideoWriter('binary.avi');
+       %open(v)
+       %writeVideo(v,M);
+       %close(v)
+       
+%Motion History Images
+t = 50;
+actionStop = 100;
+rows = readerobj.Height;
+cols = readerobj.width;
+MHI_previous = ones(rows,cols);
+MHI = ones(rows,cols);
+for i = actionStop - t + 1:actionStop
+    MHI(MHI_previous > 0) = MHI(MHI_previous > 0) - 1;
+    MHI(afterOpening(:,:,i)) = t;
+    MHI_previous = MHI;
+end
+MHI_scaled = MHI./max(MHI(:));
+%imshow(MHI_scaled);
+%imwrite(MHI_scaled,'output/MHIA1.jpeg')
